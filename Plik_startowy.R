6bed3d8c633ef2534b1a6dfe3133870cc3527d3b
lucas9999 <memy@wp.pl>
2020-11-27 10:35:44 +0100

    pierwszy commit

---
+++ Plik_startowy.R
@@ -0,0 +1,12 @@
+
+bookdown::serve_book(dir = ".", output_dir = "docs", preview = TRUE, in_session = TRUE)
+
+ # shell.exec(paste0(getwd(), '/_book/index.html' ) )
+
+
+# TO OD
+# detekcja czy jest litera ('m' in 'mmm' i wersja z regular expression )
+# dodawanie i usuwanie wymiaru z array (moze juz jest dodane)
+# bindowanie array od pustego - czy da sie zrobic bez inicjowania wymiaru)
+
+
---
+++ templates.R
@@ -0,0 +1,46 @@
+# Running
+
+bibiolgrafia :
+shell.exec('bibliography.xlsx')
+shell.exec('book.bib')
+
+katalog:
+  shell.exec('C:/Users/memy/OneDrive/Pulpit/1_Programs_and_programing/PYTHON/Manual_Python')
+
+
+
+# Bibliograpy
+bibliografia jest w pliku book.bib. Wpisy do niej sa generowane w pliku excel bibliography.xlsx. Ostatnie kolumna generuje gotowy wpis do pliku book.bib.
+Przykladowy wpis:
+  [@Odelama]
+
+
+
+# Path for git
+cd C:/Users/memy/OneDrive/Pulpit/1_Programs_and_programing/LANG_PYTHON/Manual_Python
+
+
+# Url
+[linked phrase](http://example.com)
+
+
+# Inserting image with caption -----
+
+
+99_APPENDIX/figures
+
+```{r, eval=TRUE, echo=FALSE, fig.cap='Choosing machine learning method', out.width='500px'}
+knitr::include_graphics('01_BASICS/figures/machine-learning-cheet-sheet.png')
+```
+
+
+
+# Inserting image without caption ----
+
+![](01_BASICS/figures/machine-learning-cheet-sheet.png)
+
+
+
+
+
+
